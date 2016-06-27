2d2ef17140e93fe8cd62c763a80189ae3fbf685e
王玉翠 <wangyucui@wangyucuidemacbook-air.local>
2016-06-27 09:35:04 +0800

    Initial Commit

---
+++ 推送/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  推送
+//
+//  Created by 王玉翠 on 16/6/27.
+//  Copyright © 2016年 王玉翠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

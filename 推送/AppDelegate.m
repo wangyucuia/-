2b6c729b10fc66bd3f87ddf67b284ebfda0db24f
王玉翠 <m15201386258@163.com>
2016-06-27 14:53:38 +0800

    本地推送

--- 推送/AppDelegate.m
+++ 推送/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,13 +17,77 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor orangeColor];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor magentaColor]];
+    [[UINavigationBar appearance] setBarStyle:UIBarStyleBlack];
+    ViewController *mainVC = [[ViewController alloc] init];
+    
+    self.window.rootViewController = mainVC;
+    [self.window makeKeyAndVisible];
+    if ([[UIApplication sharedApplication] currentUserNotificationSettings].types != UIUserNotificationTypeNone) {
+        [self addLocalNotification];
+    }else{
+      
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert| UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
+        
+        
+        
+    }
+    
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+
+-(void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings{
+    
+    if (notificationSettings.types != UIUserNotificationTypeNone) {
+        [self addLocalNotification];
+    }
+    
+    
 }
+-(void)applicationWillResignActive:(UIApplication *)application{
+    
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    
+    
+    
+}
+
+
+-(void)addLocalNotification{
+    
+    //定义本地通知对象
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    //设置调用时间
+    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:10.0];//通知中心调用10时s之后
+    notification.repeatInterval = 2;//重复次数
+    notification.alertBody = @"最近天气不好";
+    notification.applicationIconBadgeNumber = 1;//应用程序右上角显示的数字
+    notification.alertAction = @"打开应用";//待机界面的滑动动作提示
+    notification.alertLaunchImage = @"123";//通过点击通知打开应用时的启动图片,这里使用程序启动图片
+    notification.soundName = @"msa.caf";//通知声音（需要真机才可以听到）
+    notification.userInfo = @{@"id":@1,@"user":@"kenshu"};//绑定到通知上的其他附加信息
+    UIApplication *app = [UIApplication sharedApplication];
+    [app scheduleLocalNotification:notification];
+    
+    
+    
+    [self removeNotification];
+    
+}
+
+
+-(void)removeNotification{
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+}
+
+
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
